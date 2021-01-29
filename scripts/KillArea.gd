1f8777d2626c2dc44543ea0bb0f19757c27c430c
Ricardas Dukauskas <rts.rd2@gmail.com>
2021-01-29 21:57:05 +0100

    added blocks for jump metrics added start of saw blade trap added trigger area for player

---
+++ scripts/KillArea.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+export(float) var spin_rps = 1
+
+var _spin_speed = spin_rps * PI * 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	rotate(delta * _spin_speed)
+
+
+func _on_KillZone_area_entered(area):
+	print("KILLLLLL")
+	pass # Replace with function body.
