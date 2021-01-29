30ab4218b364dacded2cb370131852608eeac6dc
Ricardas Dukauskas <rts.rd2@gmail.com>
2021-01-30 00:31:09 +0100

    fighting the capitalizm

--- scripts/_game.gd
+++ scripts/_game.gd
@@ -1,9 +1,10 @@
 extends Node
 
 export(PackedScene) var player_class
+export(PoolStringArray) var level_paths
 
 var current_scene = null
-var current_level_id = 1
+var current_level_id = 0
 
 func _ready():
 	var root = get_tree().get_root()
@@ -14,7 +15,9 @@ func reset_level():
 
 func load_next_level():
 	current_level_id = current_level_id + 1
-	call_deferred("_deferred_goto_scene", "res://scenes/levels/level_2.tscn")
+	if level_paths.size() < current_level_id:
+		var path = level_paths[current_level_id]
+		call_deferred("_deferred_goto_scene", path)
 
 func _deferred_goto_scene(path):
 	current_scene.free()
