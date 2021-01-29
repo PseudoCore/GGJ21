ff2ed6646e64fb7b6e2eb0410c26bb845c596c36
Ricardas Dukauskas <rts.rd2@gmail.com>
2021-01-30 00:16:32 +0100

    added test level 2 with level transition

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,6 +3,7 @@ extends Node
 export(PackedScene) var player_class
 
 var current_scene = null
+var current_level_id = 1
 
 func _ready():
 	var root = get_tree().get_root()
@@ -12,4 +13,12 @@ func reset_level():
 	current_scene.call_deferred("reset_level")
 
 func load_next_level():
-	print("FREEEDOOOOM")
+	current_level_id = current_level_id + 1
+	call_deferred("_deferred_goto_scene", "res://scenes/levels/level_2.tscn")
+
+func _deferred_goto_scene(path):
+	current_scene.free()
+	var s = ResourceLoader.load(path)
+	current_scene = s.instance()
+	get_tree().get_root().add_child(current_scene)
+	get_tree().set_current_scene(current_scene)
