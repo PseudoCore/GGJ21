700163175f2053ed0146fbaec4b6e980521301dc
Ricardas Dukauskas <rts.rd2@gmail.com>
2021-01-29 20:05:56 +0100

    Player input implementation start

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,7 @@ export(float) var speed = 64
 export(float) var jump_height = 128
 export(float) var air_control_mult = 1
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var _velocity = Vector2(0, 0)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,11 +15,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_pressed("player_move_left")):
-		print("left")
+		_velocity = Vector2(-speed,0)
 	if(Input.is_action_pressed("player_move_right")):
-		print("right")
+		_velocity = Vector2(speed,0)
 	if(Input.is_action_just_pressed("player_jump")):
 		print("jump")
+		
+
+	
+func _physics_process(delta):
+	_velocity = move_and_slide(_velocity, Vector2.UP)
 
 #func _input(event):
 #	if(Input.is_action_pressed("player_move_left")):
