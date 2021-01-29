0bf5970f9008997eb825cd3f2c7cda434c146e25
devcore <jefremovasandrius@gmail.com>
2021-01-29 23:13:29 +0000

    Updating player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,53 +1,50 @@
 extends KinematicBody2D
 
-export(float) var move_speed = 64
-export(float) var jump_height = -256
-export(float) var air_control_mult = 0.25
-export (int) var gravity = 256
+export(float, 0, 1.0) var friction = 0.2
+export(float, 0, 1.0) var acceleration = 0.25
 
-var _velocity = Vector2(0, 0)
-var _jumping = false
+var move_speed = 5 * Globals.UNIT_SIZE
+var min_jump_height = 1 * Globals.UNIT_SIZE
+var max_jump_height = 4 * Globals.UNIT_SIZE
+var min_jump_speed
+var max_jump_speed
+var gravity
 
+onready var _velocity = Vector2(0, 0)
+onready var is_jumping = false
+onready var jump_duration = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	gravity = 2 * max_jump_height / pow(jump_duration, 2)
+	min_jump_speed = sqrt(2 * gravity * min_jump_height)
+	max_jump_speed = sqrt(2 * gravity * max_jump_height)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("dbp_escape")):
 		get_tree().quit()
 
-func get_physic_input():
-	var move_speed_mult = 1;
-
-	if (is_on_floor()):
-		_velocity = Vector2.ZERO
-	else:
-		move_speed_mult = air_control_mult
-
-	if(Input.is_action_pressed("player_move_left")):
-		_velocity.x -= move_speed * move_speed_mult
-	if(Input.is_action_pressed("player_move_right")):
-		_velocity.x += move_speed * move_speed_mult
-	if(Input.is_action_just_pressed("player_jump")):
-		_velocity.y += jump_height
-		_jumping = true
-
 func _physics_process(delta):
-	get_physic_input()
+	_process_physic_input(delta)
 
-	if (_jumping):
-		_jumping = false
-
-	_velocity.y += gravity * delta
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 
-#func _input(event):
-#	if(Input.is_action_pressed("player_move_left")):
-#		print("left")
-#	if(Input.is_action_pressed("player_move_right")):
-#		print("right")
-#	if(Input.is_action_just_pressed("player_jump")):
-#		print("jump")
+func _process_physic_input(delta):
+	var move_dir = Input.get_action_strength("player_move_right") - Input.get_action_strength("player_move_left")
+
+	if move_dir != 0:
+		_velocity.x = lerp(_velocity.x, move_dir * move_speed, acceleration)
+	else:
+		_velocity.x = lerp(_velocity.x, 0, friction)
+
+	_velocity.y += gravity * delta
+
+	if is_jumping && _velocity.x >= 0:
+		is_jumping = false
+
+	if (Input.is_action_just_pressed("player_jump") && is_on_floor()):
+		_velocity.y -= max_jump_speed
+
+	if (Input.is_action_just_released("player_jump") && _velocity.y <= -min_jump_speed):
+		_velocity.y = -min_jump_speed
