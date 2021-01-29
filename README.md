2ad25f4b2d03bd33a24be9e8f6917c4935dc03d6
devcore <jefremovasandrius@gmail.com>
2021-01-29 20:04:22 +0000

    Experimenting with player controls

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GGJ21
+
+## Diversifiers
+On the safe side - Make a game alone or in a team where all members are at least 100km apart.
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
-export(float) var speed = 64
-export(float) var jump_height = 128
-export(float) var air_control_mult = 1
+export(float) var move_speed = 64
+export(float) var jump_height = -256
+export(float) var air_control_mult = 0.25
+export (int) var gravity = 256
 
 var _velocity = Vector2(0, 0)
+var _jumping = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,16 +16,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_pressed("player_move_left")):
-		_velocity = Vector2(-speed,0)
-	if(Input.is_action_pressed("player_move_right")):
-		_velocity = Vector2(speed,0)
-	if(Input.is_action_just_pressed("player_jump")):
-		print("jump")
-		
+	if(Input.is_action_just_pressed("dbp_escape")):
+		get_tree().quit()
 
-	
+func get_physic_input():
+	var move_speed_mult = 1;
+
+	if (is_on_floor()):
+		_velocity = Vector2.ZERO
+	else:
+		move_speed_mult = air_control_mult
+
+	if(Input.is_action_pressed("player_move_left")):
+		_velocity.x -= move_speed * move_speed_mult
+	if(Input.is_action_pressed("player_move_right")):
+		_velocity.x += move_speed * move_speed_mult
+	if(Input.is_action_just_pressed("player_jump")):
+		_velocity.y += jump_height
+		_jumping = true
+
 func _physics_process(delta):
+	get_physic_input()
+
+	if (_jumping):
+		_jumping = false
+
+	_velocity.y += gravity * delta
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 
 #func _input(event):
