cdceaf4b11990bfe40e973cc17df402a9e24f78b
devcore <jefremovasandrius@gmail.com>
2021-01-30 00:19:01 +0000

    Adding a script file hierarchy

--- scripts/globals.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-const UNIT_SIZE = 64
-
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,24 +3,26 @@ extends KinematicBody2D
 export(float, 0, 1.0) var friction = 0.2
 export(float, 0, 1.0) var acceleration = 0.25
 
-var move_speed = 5 * Globals.UNIT_SIZE
-var min_jump_height = 1 * Globals.UNIT_SIZE
-var max_jump_height = 4 * Globals.UNIT_SIZE
+#var SLOPE_STOP = 2 * Globals.TILE_SIZE
+
+var move_speed = 5 * Globals.TILE_SIZE
+var min_jump_height = 0.8 * Globals.TILE_SIZE
+var max_jump_height = 4 * Globals.TILE_SIZE
 var min_jump_speed
 var max_jump_speed
 var gravity
+#var is_grounded
 
 onready var _velocity = Vector2(0, 0)
 onready var is_jumping = false
 onready var jump_duration = 0.5
+#onready var groundRaycasts = $GroundRayCasts
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	gravity = 2 * max_jump_height / pow(jump_duration, 2)
 	min_jump_speed = sqrt(2 * gravity * min_jump_height)
 	max_jump_speed = sqrt(2 * gravity * max_jump_height)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("dbp_escape")):
 		get_tree().quit()
@@ -29,6 +31,9 @@ func _physics_process(delta):
 	_process_physic_input(delta)
 
 	_velocity = move_and_slide(_velocity, Vector2.UP)
+	#_velocity = move_and_slide(_velocity, Vector2.UP, SLOPE_STOP)
+	
+	#is_grounded = _check_is_grounded()
 
 func _process_physic_input(delta):
 	var move_dir = Input.get_action_strength("player_move_right") - Input.get_action_strength("player_move_left")
@@ -48,3 +53,5 @@ func _process_physic_input(delta):
 
 	if (Input.is_action_just_released("player_jump") && _velocity.y <= -min_jump_speed):
 		_velocity.y = -min_jump_speed
+
+#_check_is_grounded
